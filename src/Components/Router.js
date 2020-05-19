9de7f1cbf9cbcd9c1d8efb11db1428f79845018e
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-05-19 20:45:00 +0900

    Finished Header

---
+++ src/Components/Router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom"; //BrowserRouter를 사용하면 #이 생기지 않는다.
+import Header from "Components/Header";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import TV from "Routes/TV";
+
+export default () => (
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
+);
