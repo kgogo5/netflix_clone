74bd7f38ab515627486b78e2c388125306926679
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-06-15 01:40:06 +0900

    Updates

---
+++ precache-manifest.645e1005fdc0d4e91bdab92e81b300a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060ab6ac7ef69b8523d98da06ae96cfd",
+    "url": "/netflix_clone/index.html"
+  },
+  {
+    "revision": "cf26702adebdae1bb2f0",
+    "url": "/netflix_clone/static/js/2.6451177b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix_clone/static/js/2.6451177b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4a537e41044095e760",
+    "url": "/netflix_clone/static/js/main.f77440ec.chunk.js"
+  },
+  {
+    "revision": "d1c9506ee61ea9f5781c",
+    "url": "/netflix_clone/static/js/runtime-main.e470034d.js"
+  },
+  {
+    "revision": "76cbac17ab5ddd56e519fac01ae2ce4d",
+    "url": "/netflix_clone/static/media/Metacritic_black.76cbac17.png"
+  }
+]);
